b026dcc4ae54550be00d1418581cfef1569682a8
Adiel Matuf <matufadiel@gmail.com>
2019-05-04 17:20:34 +0300

    change name

--- blockchain_blocks_analyzer/__init__.py
+++ blockchain_blocks_analyzer/__init__.py
@@ -1,6 +1,6 @@
-from .blockchaine_blocks_analyzer import get_blocks_by_date
-from .blockchaine_blocks_analyzer import get_single_block
-from .blockchaine_blocks_analyzer import to_millis
+from .blockchain_blocks_analyzer import get_blocks_by_date
+from .blockchain_blocks_analyzer import get_single_block
+from .blockchain_blocks_analyzer import to_millis
 
 from .elastic_handler import add_index
 from .elastic_handler import set_conf
