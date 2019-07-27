84ccb44a8ff046b42cf404842ffb2c91550ea934
Adiel Matuf <matufadiel@gmail.com>
2019-07-27 20:10:08 +0300

    fix bug, add address to suspicious addresses

--- blockchain_blocks_analyzer/node_builder.py
+++ blockchain_blocks_analyzer/node_builder.py
@@ -1,10 +1,12 @@
 from blockchain_track import generate_record, get_single_address
 from neo4j_handler import generate_graph, exist_in_graph, connect_db
+from blockchain_blocks_analyzer.Config import print_json_to_file
 
 
 def create_node(from_address, graph):
     json_file = get_single_address(from_address)
     node_info = generate_record(json_file)
+    print_json_to_file(filename=json_file['address'], content=node_info)
     generate_graph(graph, node_info)
 
 
