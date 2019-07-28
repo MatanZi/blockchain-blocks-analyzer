0d57dff1454a8a958a5e258ee4fb1c1c3f1f30a0
MatanZ <matanzilka@gmail.com>
2019-07-28 07:37:27 +0300

    Update node_builder.py

--- blockchain_blocks_analyzer/node_builder.py
+++ blockchain_blocks_analyzer/node_builder.py
@@ -5,8 +5,6 @@ from Config import print_json_to_file
 import queue
 
 from_address_queue = queue.Queue()
-
-
 def nodes_builder(from_address, credential):
     graph = connect_db(credential=credential)
     json_file = get_single_address(from_address)
@@ -14,8 +12,9 @@ def nodes_builder(from_address, credential):
     suspicious = node_info['suspicious']
     final_balance = node_info['final_balance']
     print_json_to_file(filename=json_file['address'], content=node_info)
-    while not (exist_in_graph(from_address, graph)) or final_balance == 0:
+    if(final_balance == 0):
         for val in suspicious:
-            from_address_queue.put(val)
+            if not(exist_in_graph(val, graph)):
+                from_address_queue.put(val)
 
     nodes_builder(from_address=from_address_queue.get(), credential=credential)
