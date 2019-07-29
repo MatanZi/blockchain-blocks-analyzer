ea3b01b71f09564f17b2470730880cc4ed9d7229
MatanZ <matanzilka@gmail.com>
2019-07-29 21:28:30 +0300

    Update node_builder.py

--- blockchain_blocks_analyzer/node_builder.py
+++ blockchain_blocks_analyzer/node_builder.py
@@ -4,6 +4,8 @@ from Config import Config, print_json_to_file
 import queue
 
 
+
+
 def create_node(from_address, graph):
     result = []
     json_file = get_single_address(from_address)
@@ -11,23 +13,22 @@ def create_node(from_address, graph):
     suspicious = node_info['suspicious']
     final_balance = json_file['final_balance']
     print_json_to_file(filename=json_file['address'], content=node_info)
-    #print(final_balance)
     if final_balance == 0:
         for address in suspicious:
             if not (exist_in_graph(address, graph)):
                 result.append(address)
 
-    #print(result)
     return result
 
 
 def nodes_builder(from_address, credential):
-    graph = connect_db(credential=credential)
     from_address_queue = queue.Queue()
-    suspicious = create_node(from_address, graph)
-    #print(type(suspicious))
-    from_address_queue.put(suspicious)
-    #print(suspicious)
+    graph = connect_db(credential=credential)
+    json_file = get_single_address(from_address)
+    node_info = generate_record(json_file)
+    suspicious = node_info['suspicious']
+    for address in suspicious:
+        from_address_queue.put(address)
     while not from_address_queue.empty():
         suspicious = create_node(from_address_queue.get(), graph)
         for address in suspicious:
